1baf8503ee4e4d9da034ad2bba3bd958e9a11b77
Ankush <ankush.program@gmail.com>
2026-09-15 18:18:02 +0530

    add clear explain of for loop execution, also make the output more clean

--- practice/12_for_while_do.cpp
+++ practice/12_for_while_do.cpp
@@ -2,10 +2,13 @@
 
 int main(){
     // for loop
-    //Syntax - for ( initialisation ; condition ; updation){   body   }
+    // Syntax: for ( initialisation ; condition ; updation){   body   }
+    // Order: initialisation (one time only) --> condition check --> executing body --> updation --> repeat
     for(int i = 1 ; i <= 6 ; i++ ){
         std::cout << i << '\n';
     }
+
+    std::cout << std::endl;
 
     // while loop
     int j = 1;
@@ -13,6 +16,8 @@ int main(){
         std::cout << j << '\n';
         j++;
     }
+
+    std::cout << std::endl;
 
     // do while loop - execute atleast one time as the condition is later
     int k = 1 ;
